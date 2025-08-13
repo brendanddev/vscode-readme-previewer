a5f51a29139edd369294c4bd84cb3b38ace38803
Brendan Dileo <dileob23@gmail.com>
2025-08-12 20:08:28 -0400

    Update extension to test

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
+/**
+ * @file extension.ts
+ * The main entry point for the README previewer VS Code extension
+ */
+
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+/**
+ * Called when the extension is activated
+ * 
+ * @param context The extension context provided by VS Code
+ */
 export function activate(context: vscode.ExtensionContext) {
+		console.log('Congratulations, your extension "vscode-readme-previewer" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-readme-previewer" is now active!');
+		const disposable = vscode.commands.registerCommand('vscode-readme-previewer.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello World from vscode-readme-previewer!');
+		});
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-readme-previewer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-readme-previewer!');
-	});
-
-	context.subscriptions.push(disposable);
+		context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+
+/**
+ * Deactivates the extension
+ */
 export function deactivate() {}
